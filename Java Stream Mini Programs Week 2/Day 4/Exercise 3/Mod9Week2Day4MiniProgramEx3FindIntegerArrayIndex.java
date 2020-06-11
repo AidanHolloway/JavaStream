4df206c8ff2cbaa31fea21a12eddf9ba68ef0a0b
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-11 21:30:56 +0200

    Update Mod9Week2Day4MiniProgramEx3FindIntegerArrayIndex.java

--- Java Stream Mini Programs Week 2/Day 4/Exercise 3/Mod9Week2Day4MiniProgramEx3FindIntegerArrayIndex.java
+++ Java Stream Mini Programs Week 2/Day 4/Exercise 3/Mod9Week2Day4MiniProgramEx3FindIntegerArrayIndex.java
@@ -1,26 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package mod9week2day4miniprogramex3findintegerarrayindex;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Aidan
- */
 public class Mod9Week2Day4MiniProgramEx3FindIntegerArrayIndex {
 
-    /**
-     * @param args the command line arguments
-     */
    public static void main(String[] args) {
         int[] intArr = {25, 14, 56, 15, 36, 56, 77, 18, 29, 49};
         
         boolean found = false;
-        System.out.println("Guess if an value is in this array of integer, if"
+        System.out.println("Guess if a value is in this array of integers, if"
                 + " you guess the right value we will say what index of the "
                 + "array it is currently kept in."); 
         Scanner input = new Scanner(System.in);
