8f3c6a2d649c7795a8c4f8ba3c8ece099ed71a2b
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-11 21:22:21 +0200

    Update Mod9Week2Day3MiniExercise2PasswordLength.java

--- Java Stream Mini Programs Week 2/Day 3/Exercise 2/Mod9Week2Day3MiniExercise2PasswordLength.java
+++ Java Stream Mini Programs Week 2/Day 3/Exercise 2/Mod9Week2Day3MiniExercise2PasswordLength.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package mod9week2day3miniexercise2passwordlength;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Aidan
- */
 public class Mod9Week2Day3MiniExercise2PasswordLength {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter a password of 8 characters consisting of letters and numbers.");
