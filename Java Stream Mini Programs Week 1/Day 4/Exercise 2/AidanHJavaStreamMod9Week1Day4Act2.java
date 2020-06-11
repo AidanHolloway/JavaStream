446ac150b95bfc4d7e6867c76e209b15bf89356f
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-11 22:10:39 +0200

    Update AidanHJavaStreamMod9Week1Day4Act2.java

--- Java Stream Mini Programs Week 1/Day 4/Exercise 2/AidanHJavaStreamMod9Week1Day4Act2.java
+++ Java Stream Mini Programs Week 1/Day 4/Exercise 2/AidanHJavaStreamMod9Week1Day4Act2.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package aidanhjavastreammod9week1day4act2;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Aidan
- */
 public class AidanHJavaStreamMod9Week1Day4Act2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        int countVowel = 0;
        
