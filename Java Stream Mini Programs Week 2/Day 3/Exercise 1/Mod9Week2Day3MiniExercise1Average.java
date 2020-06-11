4a3ebeaed85d9d7c0294033f4bc84a0c487b07dc
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-11 21:17:09 +0200

    Update Mod9Week2Day3MiniExercise1Average.java

--- Java Stream Mini Programs Week 2/Day 3/Exercise 1/Mod9Week2Day3MiniExercise1Average.java
+++ Java Stream Mini Programs Week 2/Day 3/Exercise 1/Mod9Week2Day3MiniExercise1Average.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package mod9week2day3miniexercise1average;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Aidan
- */
 public class Mod9Week2Day3MiniExercise1Average {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Input 3 numbers of any length with a space between 3 the 3 different nunnbers.");
