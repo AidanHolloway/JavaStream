18da1f20082b26158a9b35766d00fd966a8c6a44
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-11 21:11:08 +0200

    Update JavaStreamMiniProgram1.java

--- JavaStreamMiniProgram1.java
+++ JavaStreamMiniProgram1.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package javastreamminiprogram1;
-
 import java.util.Scanner;
 
-/**
- *
- * @author AidanH
- */
 public class JavaStreamMiniProgram1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Enter All to view all numbers from 1 to 10. "
                 + "Enter Odd to view all odd numbers from 1 to 10. "
