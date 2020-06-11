d73b7c73a1071f5fc336427834d2770c258df20e
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-11 21:58:07 +0200

    Update Exercise3.java

--- Java Stream Mini Programs Week 2/Day 1/Exercise 3/Exercise3.java
+++ Java Stream Mini Programs Week 2/Day 1/Exercise 3/Exercise3.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package exercise3;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Aidan
- */
 public class Exercise3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
    System.out.println("Enter a value from 1 to 7.");
