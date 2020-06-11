187a0579ac712a5cc55bf2799020e3498a6e8b5a
Aidan Holloway <interncptwex037@capaciti.org.za>
2020-06-11 21:55:30 +0200

    Update Exercise2.java

--- Java Stream Mini Programs Week 2/Day 1/Exercise 2/Exercise2.java
+++ Java Stream Mini Programs Week 2/Day 1/Exercise 2/Exercise2.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package exercise2;
-
 import java.util.Scanner;
 
-/**
- *
- * @author Aidan
- */
 public class Exercise2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         System.out.println("Enter 3 numbers to receive the largest number as the output. "
                 + "Keep spaces between seperate values.");
